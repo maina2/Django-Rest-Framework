38231bc3f053f0cf6c251706a755e0ebdffa6727
Simon Chanzu <chanzusimon6@gmail.com>
2025-01-04 04:32:20 +0300

    ip

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,6 +3,6 @@ import requests
 
 endpoint= "http://127.0.0.1:8000/api/"
 
-get_response= requests.get(endpoint,params={"abc":123},json={"query":"Hello world"})
-# print(get_response.json())
+get_response= requests.post(endpoint,params={"abc":123},json={"title":"New data"})
+print(get_response.json())
 
