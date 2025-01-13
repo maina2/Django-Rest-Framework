5c6127acfafcd32ae29a83f4eb7fc5cfbaa1c6b3
Simon Chanzu <chanzusimon6@gmail.com>
2025-01-13 18:44:24 +0300

    uploads today

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+# Custom User Manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        extra_fields.setdefault('is_active', True)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if not extra_fields.get("is_staff"):
+            raise ValueError("Superuser must have is_staff=True.")
+        if not extra_fields.get("is_superuser"):
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self.create_user(email=email, password=password, **extra_fields)
+
+# Custom User Model
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.CharField(max_length=80, unique=True)
+    username = models.CharField(max_length=45)
+    date_of_birth = models.DateField(null=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.username
--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'posts',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework'
 ]
+
+AUTH_USER_MODEL= "accounts.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
